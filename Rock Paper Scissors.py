431dd904cad8a36976efad17bd850e9a8b27eb4b
Hafeez Husain Bello <105268549+hhbello@users.noreply.github.com>
2022-06-10 22:31:21 +0800

    Update Rock Paper Scissors.py

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -7,14 +7,14 @@ while True:
     playerName = input("Enter player name: ")
     print("\n")
 
-    playerChoice = input("Enter rock, paper or scissors: ")
+    playerChoice = input("Enter rock, paper or scissors: ").lower()
 
     # Player's choice.
     if playerChoice not in ("rock", "paper", "scissors"):
         print("Invalid Selection! Please enter Rock, Paper or Scissors...")
 
     # Computer's Choice.
-    computerChoice = random.choice(["rock", "paper", "scissors"])
+    computerChoice = random.choice(["rock", "paper", "scissors"]).lower()
     if computerChoice == "rock":
         print(f"Computer chose {computerChoice}.")
     if computerChoice == "paper":
@@ -29,24 +29,24 @@ while True:
     # Conditions to determine winner.
     if playerChoice == "rock" and computerChoice == "paper":
         print("Paper covers Rock")
-        print("CPU WINS!!!")
+        print("CPU WON!!!")
     if playerChoice == "paper" and computerChoice == "rock":
         print("Paper covers Rock")
-        print(f"{playerName} WINS!!!")
+        print(f"{playerName} WON!!!")
 
     if playerChoice == "scissors" and computerChoice == "rock":
         print("Rock smashes Scissors")
-        print("CPU WINS!!!")
+        print("CPU WON!!!")
     if playerChoice == "rock" and computerChoice == "scissors":
         print("Rock smashes Scissors")
-        print(f"{playerName} WINS!!!")
+        print(f"{playerName} WON!!!")
 
     if playerChoice == "paper" and computerChoice == "scissors":
         print("Scissors cut Paper")
-        print("CPU WINS!!!")
+        print("CPU WON!!!")
     if playerChoice == "scissors" and computerChoice == "paper":
         print("Scissors cut Paper")
-        print(f"{playerName} WINS!!!")
+        print(f"{playerName} WON!!!")
 
     exit = input("\nWould you like to play again? Y/N: ")
     if exit == "N":
