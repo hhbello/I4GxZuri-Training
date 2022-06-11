953d1933ebe873aec875d27cbacb45c335989b75
Hafeez Husain Bello <105268549+hhbello@users.noreply.github.com>
2022-06-11 18:02:42 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+import random
+
+print("\n")
+print("<<<<<<<< Rock, Paper & Scissors: A Zuri Training Python Task >>>>>>>>")
+print("\n")
+
+while True:
+    choices = ["R", "P", "S"]
+
+    # CPU and Player possible options
+    CPUchoice = random.choice(choices).upper()
+    Playerchoice = input("R, P, or S: ").upper()
+
+    # Loop for invalid option
+    while Playerchoice not in choices:
+        print("Invalid option! Try again.")
+        Playerchoice = input("R, P, or S: ").upper()
+
+    # Tie condition
+    if Playerchoice == CPUchoice:
+        print(f"Player ({Playerchoice}) : CPU ({CPUchoice}) ")
+        print("Tie!")
+
+    # Conditions to determine winner
+    elif Playerchoice == "R":
+        if CPUchoice == "P":
+            print("Player (Rock) : CPU (Paper)")
+            print("Paper covered Rock")
+            print("CPU Won!")
+
+        if CPUchoice == "S":
+            print("Player (Rock) : CPU (Scissors)")
+            print("Rock smashed Scissors")
+            print("Player Won!")
+
+    elif Playerchoice == "P":
+        if CPUchoice == "S":
+            print("Player (Paper) : CPU (Scissors)")
+            print("Scissors cut Paper")
+            print("CPU Won!")
+        if CPUchoice == "R":
+            print("Player (Paper) : CPU (Rock)")
+            print("Paper covered Rock")
+            print("Player Won!")
+
+    elif Playerchoice == "S":
+        if CPUchoice == "R":
+            print("Player (Scissors) : CPU (Rock)")
+            print("Rock smashed Scissors")
+            print("CPU Won!")
+        if CPUchoice == "P":
+            print("Player (Scissors) : CPU (Paper)")
+            print("Scissors cut paper")
+            print("Player Won!")
+
+
+
+    play_again = input("Play again? (y/n): ").lower()
+
+    if play_again != "y":
+        break
+
+print("Goodbye!")
+
